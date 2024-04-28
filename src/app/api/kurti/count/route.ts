60f829147472058113f94a98705b9b49a2863073
Dhruvil Kakadiya <kakadiyadhruvil3006@gmail.com>
2024-04-28 12:06:30 +0530

    a bug in count API fixed

--- src/app/api/kurti/count/route.ts
+++ src/app/api/kurti/count/route.ts
@@ -6,7 +6,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     try {
-        const cat = request.nextUrl.searchParams.get("cat")
+        const cat = request.nextUrl.searchParams.get("cat") || "";
         const data = await getKurtiCount(cat);
         return new NextResponse(JSON.stringify({ data }), { status: 200 });
     } catch (error: any) {
