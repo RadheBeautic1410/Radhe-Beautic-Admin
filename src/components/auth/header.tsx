9ae3a56ca9d419e0808bf9da70b588396061131f
GarvGojariya <gsgojariya196003@gmail.com>
2025-10-30 21:17:04 +0530

    Refactor navbar and header components for improved code readability and consistency. Consolidate role arrays and update logo image in the navbar and header. Remove commented-out code for cleaner implementation.

--- src/components/auth/header.tsx
+++ src/components/auth/header.tsx
@@ -1,32 +1,39 @@
-import { cn } from "@/src/lib/utils"
-import { Poppins } from "next/font/google"
+import { cn } from "@/src/lib/utils";
+import { Poppins } from "next/font/google";
 import Logo from "../logo";
+import Image from "next/image";
+import Link from "next/link";
 
 const font = Poppins({
-    subsets: ['latin'],
-    weight: ['600']
-})
-
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 interface HeaderProps {
-    label: string;
+  label: string;
 }
 
 export const Header = ({ label }: HeaderProps) => {
-    return (
-        <div className="w-full flex flex-col gap-y-2 items-center justify-center">
-            <div className="flex flex-row items-center">
-                <Logo/>
+  return (
+    <div className="w-full flex flex-col gap-y-2 items-center justify-center">
+      <div className="flex flex-row items-center">
+        {/* <Logo/>
                 <h1 className={
                     cn("text-3xl font-semibold", font.className)
                 }>
                     <a href="/">RADHE BEUTIC</a>
-                </h1>
-            </div>
+                </h1> */}
+        <Link href="/">
+          <Image
+            src="/images/radhe_logo.svg"
+            alt="logo"
+            width={250}
+            height={250}
+          />
+        </Link>
+      </div>
 
-            <p className="text-foreground text-md">
-                {label}
-            </p>
-        </div>
-    )
-}
+      <p className="text-foreground text-md">{label}</p>
+    </div>
+  );
+};
